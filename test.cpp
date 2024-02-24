112150f9a4639ee124cfd85b87cb39a5203d7033
Mannvika <mannyjainlakes@gmail.com>
2024-02-24 16:38:48 -0500

    Began creating code format, need to fix positions.

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include <string>
+
+class Skills{};
+class Projects{};
+
+using namespace std;
+
+class ManavSanghvi{
+    string titles[3] = {
+        "Software Engineer."
+        "Game Developer."
+        "Lifelong Learner."
+    };
+
+    string education = "Bachelors in Computer Science at The University of Florida"; // Expected Dec 2026
+    string bio = "Passionate developer eager to find myself doing work I love anywhere in the world.";
+
+    Skills GetSkills(); // Click me!
+    Projects GetProjects(); // Click me!
+
+
+};
