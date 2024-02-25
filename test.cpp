b2535e723decf9974192127cf433d2235bcd5640
Mannvika <mannyjainlakes@gmail.com>
2024-02-24 21:36:44 -0500

    Removed Test files and added links

--- test.cpp
+++ test.cpp
@@ -2,10 +2,12 @@
 
 class Skills{};
 class Projects{};
+class Goals{};
 
 using namespace std;
 
 class ManavSanghvi{
+
     string titles[3] = {
         "Software Engineer."
         "Game Developer."
@@ -15,8 +17,12 @@ class ManavSanghvi{
     string education = "Bachelors in Computer Science at The University of Florida"; // Expected Dec 2026
     string bio = "Passionate developer eager to find myself doing work I love anywhere in the world.";
 
+    string email = "manavsanghvi423@gmail.com";
+    string linkedin = "";
+    string github = "";
+
     Skills GetSkills(); // Click me!
     Projects GetProjects(); // Click me!
-
+    Goals GetGoals(); // Click Me!
 
 };
