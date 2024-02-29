47cb32ab4a747d89311d88890104e15a489db138
Mannvika <45762765+Mannvika@users.noreply.github.com>
2024-02-29 00:58:41 -0500

    Added all icons and updated spacing.

--- index.js
+++ index.js
@@ -12,13 +12,25 @@ function plusDivs(direction) {
       title: "Linked",
       iconSrcs: ["images/icons/Unity.svg", "images/icons/CSharp.svg"],
       contentSrcs: ["images/LinkedPng.png", "images/LinkedPng.png"],
-      description: "1st place overall game jam winner turned fully developed Android/IOS game."
+      description: "1st place overall game jam winner refactored to become a fully developed Android/IOS game (Currently Unavailable)."
     },
     {
       title: "RGB Platformer",
-      iconSrcs: ["images/icons/Python.svg", "images/icons/Unity.svg", "images/icons/CSharp.svg", "images/icons/OrangePi.png"],
+      iconSrcs: ["images/icons/Python.png", "images/icons/Unity.svg", "images/icons/CSharp.svg", "images/icons/OrangePi.png", "images/icons/arduino.png"],
       contentSrcs: ["images/LinkedPng.png", "images/LinkedPng.png"],
-      description: "Developed a game in Unity/C# that communicates with a single-board computer and Arduino to change the color of a physical RGB LED strip via Python. "
+      description: "Hardware/software game tech demo that communicates with a single-board computer to change the color of a physical RGB LED strip via TCP listening."
+    },
+    {
+      title: "Minesweeper (School Project)",
+      iconSrcs: ["images/icons/C++.svg", "images/icons/SFML.png"],
+      contentSrcs: ["images/LinkedPng.png", "images/LinkedPng.png"],
+      description: "Recreation of the minesweeper game with custom debugging functionality through the use of object-oriented programming and singleton design patterns."
+    },
+    {
+      title: "This Website!",
+      iconSrcs: ["images/icons/Html.svg", "images/icons/CSS.svg", "images/icons/javascript.svg"],
+      contentSrcs: ["images/LinkedPng.png", "images/LinkedPng.png"],
+      description: "Web portfolio to display my projects, skills, experience, and goals."
     },
     // Add more projects as needed
   ];
